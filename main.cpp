29f16bb3d64e7086aec754a3c0ac56e45de852da
almkuznetsov <almkuznetsov@mail.ru>
2023-11-13 02:46:08 +0300

    CMakeLists upgraded
    upgraded CMakeLists to use .so files instead of .dylib ones, added requirement for boost 1.76 because of json.hpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,5 @@
 #include "lib/include/comparepackages.hpp"
 
-boost::json::value readJsonFile(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file: " << filename << std::endl;
-        return boost::json::value();
-    }
-
-    boost::json::value json;
-    try {
-        file >> json;
-    } catch (const std::exception& e) {
-        std::cerr << "Error parsing JSON: " << e.what() << std::endl;
-    }
-
-    file.close();
-    return json;
-}
-
 int main(int argc, char* argv[]) {
     std::string branch1 = "sisyphus";
     std::string branch2 = "p10";
